5a87638748e0273e540bb68041b8db0f6b709eff
Touseef Ahmad <touseefahmed9669@gmail.com>
2024-12-30 12:21:09 +0500

    Playground for financial agent (chatbot)

--- playground.py
+++ playground.py
@@ -1,13 +1,21 @@
+import os
+
+import phi
+import phi.api
+from dotenv import load_dotenv
 from phi.agent import Agent
 from phi.model.groq import Groq
-from phi.tools.yfinance import YFinanceTools
+from phi.playground import Playground, serve_playground_app
 from phi.tools.duckduckgo import DuckDuckGo
-from dotenv import load_dotenv
+from phi.tools.yfinance import YFinanceTools
 
 load_dotenv()
 
+# initialize phi api
+phi.api = os.getenv("PHI_API_KEY")
+
 # Initialize Groq Model
-groq_model = Groq(id="llama3-groq-8b-8192-tool-use-preview")
+groq_model = Groq(id="llama3-groq-70b-8192-tool-use-preview")
 
 # Web Search Agent
 websearch_agent = Agent(
@@ -38,19 +46,7 @@ financial_agent = Agent(
     markdown=True,
 )
 
-# Multi-Agent Setup
-multi_ai_agent = Agent(
-    team=[financial_agent, websearch_agent],
-    model=groq_model,
-    instructions=[
-        "Always include the source of the information in the response.",
-        "Use Tables to display the data.",
-    ],
-    show_tool_calls=True,
-    markdown=True,
-)
+app = Playground(agents=[financial_agent, websearch_agent]).get_app()
 
-# Test the Agent
-multi_ai_agent.print_response(
-    "Summarize analyst recommendations and share the latest news for NVDA.", stream=True
-)
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
